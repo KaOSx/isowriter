43859d0fc9c61a91ef1be7d8609d4cc8831de3af
demmm <anke62@gmail.com>
2016-08-14 20:28:33 -0400

    adding es, merge transifex

--- lang/isowriter-es.ts
+++ lang/isowriter-es.ts
@@ -1,4 +1,4 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="es_AR" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="es" version="2.1">
 <context>
     <name>ImageWriter</name>
     <message>
